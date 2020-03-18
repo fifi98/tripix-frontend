e22478f966b0b32b7da0c0805f81f099cb1c9731
fifi98 <bel.filip@hotmail.com>
2020-03-18 16:15:44 +0100

    Added react-native-maps (showing Apple maps at the moment)

--- screens/FindNearby.js
+++ screens/FindNearby.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { View } from "react-native";
+import MapView from "react-native-maps";
 
 const FindNearby = () => {
-  return <View></View>;
+  return (
+    <MapView
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+      style={{ flex: 1 }}
+    />
+  );
 };
 
 export default FindNearby;

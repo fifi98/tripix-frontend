9574d89a3e2b8e399e5eaa618f288069026113ae
fifi98 <bel.filip@hotmail.com>
2020-03-21 16:35:08 +0100

    Finding nearby restaurants done

--- screens/FindNearby.js
+++ screens/FindNearby.js
@@ -1,41 +1,58 @@
-import React, { useEffect, useContext } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useEffect, useContext, useState } from "react";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
 import api from "../utils/api";
 import { MyContext } from "../context/Provider";
+import { Text, Alert } from "react-native";
 
 const FindNearby = () => {
   const user = useContext(MyContext);
-
-  const data = {
-    long: 46.3059708,
-    lat: 16.3369023
-  };
+  const [userPosition, setUserPosition] = useState();
+  const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    api
-      .get("/nearby/restaurants", {
-        headers: {
-          Authorization: "Bearer " + user.token,
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        },
-        params: data
-      })
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error));
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        let initialPosition = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.035
+        };
+        setUserPosition(initialPosition);
+
+        api
+          .get("/nearby/restaurants", {
+            headers: {
+              Authorization: "Bearer " + user.token,
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            params: { long: position.coords.longitude, lat: position.coords.latitude }
+          })
+          .then(response => {
+            response.data.results.map(place => {
+              setPlaces(staro => [
+                ...staro,
+                { latitude: place.geometry.location.lat, longitude: place.geometry.location.lng, name: place.name }
+              ]);
+            });
+          })
+          .catch(error => console.log(error));
+      },
+      error => Alert.alert(error.message)
+    );
   }, []);
 
   return (
-    <MapView
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-      provider={PROVIDER_GOOGLE}
-      style={{ flex: 1 }}
-    />
+    <MapView initialRegion={userPosition} showsUserLocation={true} provider={PROVIDER_GOOGLE} style={{ flex: 1 }}>
+      {places.map(place => (
+        <Marker key={Math.random()} coordinate={{ latitude: place.latitude, longitude: place.longitude }}>
+          <Callout>
+            <Text>{place.name}</Text>
+          </Callout>
+        </Marker>
+      ))}
+    </MapView>
   );
 };
 

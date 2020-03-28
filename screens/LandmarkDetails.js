5058aa6e8646e1201b6b204a43148404aa2f2509
fifi98 <bel.filip@hotmail.com>
2020-03-28 21:32:22 +0100

    Added gradient background image to landmark details screen

--- screens/LandmarkDetails.js
+++ screens/LandmarkDetails.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const LandMarkDetails = () => {
-  return <View></View>;
+  return (
+    <ImageBackground
+      source={{
+        url: "https://handluggageonly.co.uk/wp-content/uploads/2018/10/Hand-Luggage-Only-12.jpg"
+      }}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <LinearGradient
+        colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.7)", "#000"]}
+        style={{ flex: 1 }}
+        locations={[0.2, 0.45, 0.89]}
+      >
+        <Text>London</Text>
+      </LinearGradient>
+    </ImageBackground>
+  );
 };
 
 export default LandMarkDetails;

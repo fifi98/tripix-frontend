5a232bf9a63953a700d09b452d439960b3c5e108
fifi98 <bel.filip@hotmail.com>
2020-04-04 19:01:26 +0200

    Added landmark ratings

--- components/LandmarkCard.js
+++ components/LandmarkCard.js
@@ -2,14 +2,16 @@ import React from "react";
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faCheckCircle, faStar, faStarHalfAlt, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegulat } from "@fortawesome/free-regular-svg-icons";
+import Rating from "../components/Rating";
 
 const RouteCard = ({ item, onLongPress }) => {
   return (
     <TouchableOpacity style={styles.container} onLongPress={onLongPress}>
       <ImageBackground
         source={{
-          url: "http://31.220.45.114/tripix/public/api/getphoto?photo_reference=" + item.photo_reference
+          url: "http://31.220.45.114/tripix/public/api/getphoto?photo_reference=" + item.photo_reference,
         }}
         style={{ width: "100%", height: "100%", opacity: 0.5 }}
       ></ImageBackground>
@@ -19,11 +21,13 @@ const RouteCard = ({ item, onLongPress }) => {
             <FontAwesomeIcon icon={faCheckCircle} style={styles.icon} size={24} />
           </View>
           <View style={styles.rating}>
+            <Rating rating={item.rating} />
+
+            {/* <FontAwesomeIcon icon={faStarRegulat} style={styles.icon} />
+            <FontAwesomeIcon icon={faStarHalfAlt} transform={{ flipX: 1 }} style={styles.icon} />
             <FontAwesomeIcon icon={faStar} style={styles.icon} />
             <FontAwesomeIcon icon={faStar} style={styles.icon} />
-            <FontAwesomeIcon icon={faStar} style={styles.icon} />
-            <FontAwesomeIcon icon={faStar} style={styles.icon} />
-            <FontAwesomeIcon icon={faStarHalf} transform={{ scaleX: -1 }} style={styles.icon} />
+            <FontAwesomeIcon icon={faStar} style={styles.icon} /> */}
           </View>
         </View>
         <View style={{ alignItems: "center", justifyContent: "center" }}>
@@ -49,35 +53,35 @@ const styles = StyleSheet.create({
     height: 120,
     marginVertical: 8,
     borderRadius: 10,
-    overflow: "hidden"
+    overflow: "hidden",
   },
   data: {
     position: "absolute",
     width: "100%",
-    height: "100%"
+    height: "100%",
   },
   textContainer: {
     alignItems: "center",
     justifyContent: "center",
     width: "80%",
-    height: "100%"
+    height: "100%",
   },
   rating: {
     padding: 4,
     position: "absolute",
     right: 10,
     top: 90,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   checkBox: {
     padding: 4,
     position: "absolute",
     left: 10,
     top: 10,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   icon: {
-    color: "white"
+    color: "white",
   },
   textName: {
     fontSize: 20,
@@ -85,8 +89,8 @@ const styles = StyleSheet.create({
     color: "white",
     textShadowColor: "black",
     textShadowRadius: 10,
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
 
 export default RouteCard;
--- components/Rating.js
+++ components/Rating.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegulat } from "@fortawesome/free-regular-svg-icons";
 
-const Rating = ({rating}) => {
-    return (  );
-}
- 
+const Rating = ({ rating }) => {
+  let icons = [];
+
+  let fullStars = Math.floor(rating);
+  let halfStars = rating % 1;
+  if (halfStars != 0) {
+    if (halfStars > 0.8) {
+      fullStars++;
+      halfStars = 0;
+    } else halfStars = 1;
+  }
+
+  const blankStars = 5 - (fullStars + halfStars);
+
+  for (let i = 0; i < blankStars; i++) icons.push(<FontAwesomeIcon icon={faStarRegulat} style={styles.icon} />);
+  for (let i = 0; i < halfStars; i++) icons.push(<FontAwesomeIcon icon={faStarHalfAlt} style={styles.icon} transform={{ flipX: 1 }} />);
+  for (let i = 0; i < fullStars; i++) icons.push(<FontAwesomeIcon icon={faStar} style={styles.icon} />);
+
+  return <View style={{ flexDirection: "row" }}>{icons}</View>;
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    color: "white",
+  },
+});
+
 export default Rating;

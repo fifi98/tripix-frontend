d8651429cc8c1e29cc375def1c8ba3f21d3b4662
fifi98 <bel.filip@hotmail.com>
2020-04-30 23:24:44 +0200

    Using InteractionManager for screens where maps are used for smooth screen transition

--- screens/routes/new/Trip.js
+++ screens/routes/new/Trip.js
@@ -1,15 +1,19 @@
-import React, { createRef } from "react";
+import React, { createRef, useEffect, useState } from "react";
+import { InteractionManager } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Polyline, Marker } from "react-native-maps";
 import { faChevronCircleLeft, faLandmark, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { mapStyle } from "../../../constants/mapStyle";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Sheet from "../../../components/Sheet";
 import LandmarkItem from "../../../components/Route/Overview/LandmarkItem";
+import { colors } from "../../../constants/theme";
+import Loading from "../../../components/Loading";
 
 const Trip = ({ navigation, route }) => {
   const { trip } = route.params;
+  const [loading, setLoading] = useState(true);
 
   let mapRef = createRef();
 
@@ -17,51 +21,66 @@ const Trip = ({ navigation, route }) => {
     navigation.navigate("PlannedRoutes");
   };
 
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
-      <View>
-        <Sheet />
-      </View>
-      <MapView
-        onMapReady={() => {
-          mapRef.fitToCoordinates(trip.locations, {
-            edgePadding: { top: 0, right: 50, bottom: 200, left: 50 },
-            animated: true,
-          });
-        }}
-        ref={(ref) => (mapRef = ref)}
-        provider={PROVIDER_GOOGLE}
-        style={{ flex: 1 }}
-        customMapStyle={mapStyle}
-      >
-        {/* Draw the route */}
-        <Polyline coordinates={trip.locations} strokeWidth={5} strokeColor="#3890FB" />
+      {loading ? (
+        <View style={{ flex: 1, backgroundColor: colors.background, justifyContent: "center", alignItems: "center" }}>
+          <Loading text="Loading trip" />
+        </View>
+      ) : (
+        <>
+          <View>
+            <Sheet />
+          </View>
+          <MapView
+            onMapReady={() => {
+              mapRef.fitToCoordinates(trip.locations, {
+                edgePadding: { top: 0, right: 50, bottom: 200, left: 50 },
+                animated: true,
+              });
+              setLoading(false);
+            }}
+            ref={(ref) => (mapRef = ref)}
+            provider={PROVIDER_GOOGLE}
+            style={{ flex: 1 }}
+            customMapStyle={mapStyle}
+          >
+            {/* Draw the route */}
+            <Polyline coordinates={trip.locations} strokeWidth={5} strokeColor="#3890FB" />
 
-        {/* Mark all the locations */}
-        {trip.locations.map((loc, index) => (
-          <Marker key={loc.latitude} tracksViewChanges={false} coordinate={{ latitude: loc.latitude, longitude: loc.longitude }}>
-            {index == 0 || index == trip.locations.length - 1 ? (
-              <FontAwesomeIcon icon={faMapMarkerAlt} size={30} style={{ color: "white" }} />
-            ) : (
-              <View style={{ backgroundColor: "#30D158", padding: 6, borderRadius: 20 }}>
-                <FontAwesomeIcon icon={faLandmark} size={18} style={{ color: "white" }} />
-              </View>
-            )}
-          </Marker>
-        ))}
-      </MapView>
-      <Sheet title="Route overview" buttonText="Start route">
-        <ScrollView>
-          {trip.locations.map((location) => (
-            <LandmarkItem location={location} key={location.latitude} />
-          ))}
-        </ScrollView>
-      </Sheet>
-      <View style={styles.iconContainer}>
-        <TouchableOpacity onPress={handleBack}>
-          <FontAwesomeIcon icon={faChevronCircleLeft} style={styles.icon} size={34} />
-        </TouchableOpacity>
-      </View>
+            {/* Mark all the locations */}
+            {trip.locations.map((loc, index) => (
+              <Marker key={loc.latitude} tracksViewChanges={false} coordinate={{ latitude: loc.latitude, longitude: loc.longitude }}>
+                {index == 0 || index == trip.locations.length - 1 ? (
+                  <FontAwesomeIcon icon={faMapMarkerAlt} size={30} style={{ color: "white" }} />
+                ) : (
+                  <View style={{ backgroundColor: "#30D158", padding: 6, borderRadius: 20 }}>
+                    <FontAwesomeIcon icon={faLandmark} size={18} style={{ color: "white" }} />
+                  </View>
+                )}
+              </Marker>
+            ))}
+          </MapView>
+          <Sheet title="Route overview" buttonText="Start route">
+            <ScrollView>
+              {trip.locations.map((location) => (
+                <LandmarkItem location={location} key={location.latitude} />
+              ))}
+            </ScrollView>
+          </Sheet>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity onPress={handleBack}>
+              <FontAwesomeIcon icon={faChevronCircleLeft} style={styles.icon} size={34} />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   );
 };
--- screens/user/FindNearby.js
+++ screens/user/FindNearby.js
@@ -4,69 +4,81 @@ import Geolocation from "@react-native-community/geolocation";
 import api from "../../utils/api";
 import Sheet from "../../components/Sheet";
 import NearbyItem from "../../components/NearbyItem";
-import { View, Alert, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { View, Alert, StyleSheet, TouchableOpacity, ScrollView, InteractionManager, Text } from "react-native";
 import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { mapStyle } from "../../constants/mapStyle";
+import { colors } from "../../constants/theme";
+import Loading from "../../components/Loading";
 
 const FindNearby = ({ route, navigation }) => {
   const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { type, icon, color } = route.params;
   let mapRef = createRef();
 
   // Load nearby places when the screen is loaded
   useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        api
-          .get(`/nearby/${type}`, {
-            params: { long: position.coords.longitude, lat: position.coords.latitude },
-          })
-          .then((response) => {
-            setPlaces(response.data);
-          });
-      },
-      (error) => Alert.alert(error.message)
-    );
-  }, []);
-
-  // Fit map to all places after they have been loaded
-  useEffect(() => {
-    mapRef.fitToCoordinates(places, {
-      edgePadding: { top: 0, right: 50, bottom: 200, left: 50 },
-      animated: true,
+    InteractionManager.runAfterInteractions(() => {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          api
+            .get(`/nearby/${type}`, {
+              params: { long: position.coords.longitude, lat: position.coords.latitude },
+            })
+            .then((response) => {
+              setPlaces(response.data);
+              setLoading(false);
+            });
+        },
+        (error) => Alert.alert(error.message)
+      );
     });
-  }, [places]);
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
-      <MapView
-        ref={(ref) => (mapRef = ref)}
-        showsUserLocation={true}
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        customMapStyle={mapStyle}
-      >
-        {places.map((place) => (
-          <Marker key={place.place_id} tracksViewChanges={false} coordinate={{ latitude: place.latitude, longitude: place.longitude }}>
-            <View style={{ ...styles.markerContainer, backgroundColor: color }}>
-              <FontAwesomeIcon icon={icon} size={18} style={styles.icon} />
-            </View>
-          </Marker>
-        ))}
-      </MapView>
-      <Sheet title="Nearby" buttonText="Show All">
-        <ScrollView>
-          {places.map((place) => (
-            <NearbyItem item={place} />
-          ))}
-        </ScrollView>
-      </Sheet>
-      <View style={styles.iconContainer}>
-        <TouchableOpacity onPress={navigation.goBack}>
-          <FontAwesomeIcon icon={faChevronCircleLeft} style={styles.icon} size={34} />
-        </TouchableOpacity>
-      </View>
+      {loading ? (
+        <View style={{ flex: 1, backgroundColor: colors.background, justifyContent: "center", alignItems: "center" }}>
+          <Loading text="Loading places" />
+        </View>
+      ) : (
+        <>
+          <MapView
+            ref={(ref) => (mapRef = ref)}
+            onMapReady={() => {
+              mapRef.fitToCoordinates(places, {
+                edgePadding: { top: 0, right: 50, bottom: 200, left: 50 },
+                animated: true,
+              });
+            }}
+            showsUserLocation={true}
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            customMapStyle={mapStyle}
+          >
+            {places.map((place) => (
+              <Marker key={place.place_id} tracksViewChanges={false} coordinate={{ latitude: place.latitude, longitude: place.longitude }}>
+                <View style={{ ...styles.markerContainer, backgroundColor: color }}>
+                  <FontAwesomeIcon icon={icon} size={18} style={styles.icon} />
+                </View>
+              </Marker>
+            ))}
+          </MapView>
+          <Sheet title="Nearby" buttonText="Show All">
+            <ScrollView>
+              {places.map((place) => (
+                <NearbyItem key={place.place_id} item={place} />
+              ))}
+            </ScrollView>
+          </Sheet>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity onPress={navigation.goBack}>
+              <FontAwesomeIcon icon={faChevronCircleLeft} style={styles.icon} size={34} />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   );
 };

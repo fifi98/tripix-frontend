f26fdf2b96405ffa8b669565d5601132e14a9a0b
fifi98 <bel.filip@hotmail.com>
2020-05-16 21:07:02 +0200

    Added back button for Android that looks like the on on iOS

--- components/ui/BackButton.js
+++ components/ui/BackButton.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform, Text, TouchableOpacity } from "react-native";
 import { HeaderBackButton } from "@react-navigation/stack";
 
 const BackButton = ({ onPress }) => {
+  if (Platform.OS === "ios")
+    return (
+      <View style={styles.container}>
+        <HeaderBackButton onPress={onPress} />
+      </View>
+    );
+
   return (
-    <View style={styles.container}>
-      <HeaderBackButton onPress={onPress} />
+    <View style={styles.androidContainer}>
+      <TouchableOpacity onPress={onPress}>
+        <Text style={styles.text}>Back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,6 +22,13 @@ const BackButton = ({ onPress }) => {
 const styles = StyleSheet.create({
   container: {
     marginLeft: -10,
+    marginBottom: 2,
+  },
+  text: {
+    fontSize: 18,
+    color: "#0077F9",
+  },
+  androidContainer: {
     marginBottom: 2,
   },
 });
